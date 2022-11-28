1cb3c8bc5613351e5709459d266f0276386eaf3c
juelhossain <juelhossainnn@gmail.com>
2022-11-28 22:59:43 +0600

    bug fixed on adpage

--- src/pages/ad/GiveAnAd.jsx
+++ src/pages/ad/GiveAnAd.jsx
@@ -5,14 +5,14 @@ import PayForAd from "./PayForAd";
 import SellerProducts from "./SellerProducts";
 
 export default function GiveAnAd() {
-  const { id } = useAdFormContext();
+  const { values: { id } = {} } = useAdFormContext();
   return (
     <Container className="my-5 sm:my-10 space-y-8 ">
       <Title align="center" order={2}>
         Please Select An Ad Package For Your Product
       </Title>
       <Package />
-      
+
       <SellerProducts />
       {id && <PayForAd />}
     </Container>
